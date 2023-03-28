e9561af82897b1f5b13c782231a69d0f461c30e6
Mohapatra jitesh <121149494+jiteshmohapatra@users.noreply.github.com>
2023-03-28 14:58:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+    <html>
+     
+        <body>
+            
+
+<?php
+
+echo "<html>";
+echo "<head>";
+echo "<style>";
+echo "body {  text-align:center; font-style:italic; padding:35px; background-color:pink; display:inline-block; font-family:timesnewroman; font-size:25px;}"; // set the background color to light gray
+ // set the background color to light gray
+
+echo "h1{ color: red; }"; // set the font color of paragraphs to blue
+echo "h2 { color: blue; }"; // set the font color of paragraphs to blue
+ // set the font color of paragraphs to blue
+ 
+echo "</style>";
+echo "</head>";
+echo "<body>";
+
+echo "<h1><u>Welcome to my 1st php programm page ::</u></h1>";
+
+
+
+define("name","jitesh <b style=color:yellow>mohapatra</b>");
+define("address","bhubaneswer<b style=color:yellow;>(Odisha)</b>");
+define("email","jiteshmohapatra<b style=color:yellow;>@gmail.com</b>");
+define("phone","<b style=color:yellow;>9116162445</b>");
+define("adit","Adit-2022");
+echo "<h2>my name is -".name.",<br> ";
+echo "my address is -".address.",<br>";
+echo "my email name is -".email.",<br>";
+echo "my phone no is -".phone.",<br>";
+echo "my adit registration no is -<b style=color:yellow;>".adit."</b>,<br></h2><hr size=15 noshade>";
+
+echo "</body>";
+echo "</html>";
+?>
+
+</body>
+</html>
